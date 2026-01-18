29a914bd6f6f0a9499bfee0754da197710a10aa9
hamjiwon <skxo0039@gmail.com>
2026-01-18 16:27:36 +0900

    x만큼 간격이 있는 n개의 숫자

---
+++ src/level1/Q12954.java
@@ -0,0 +1,15 @@
+package level1;
+
+public class Q12954 {
+    public long[] solution(int x, int n) {
+        long[] answer = new long[n];
+
+        long xx = x;
+        for (int i = 0; i < n; i++) {
+            answer[i] = xx;
+            xx += x;
+        }
+
+        return answer;
+    }
+}
