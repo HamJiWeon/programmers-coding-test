ce23f2bca26b67de8c1042e9a60537e72b028cd9
hamjiwon <skxo0039@gmail.com>
2026-01-22 23:01:42 +0900

    하샤드 수

---
+++ src/level1/Q12947.java
@@ -0,0 +1,16 @@
+package level1;
+
+public class Q12947 {
+    public boolean solution(int x) {
+
+        int sum = 0;
+        int n = x;
+
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+
+        return x % sum == 0;
+    }
+}
