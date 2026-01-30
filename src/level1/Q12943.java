e997207d9be02a995fa017289f671f379125d4c4
hamjiwon <skxo0039@gmail.com>
2026-01-30 23:44:59 +0900

    콜라츠 추측

---
+++ src/level1/Q12943.java
@@ -0,0 +1,22 @@
+package level1;
+
+public class Q12943 {
+    public int solution(int num) {
+        int answer = 0;
+
+        long n = (long) num;
+
+        if (n == 1) return 0;
+
+        while (n != 1) {
+            if (answer == 500) return -1;
+
+            if (n % 2 == 0) n /= 2;
+            else n = (n * 3) + 1;
+
+            answer++;
+        }
+
+        return answer;
+    }
+}
