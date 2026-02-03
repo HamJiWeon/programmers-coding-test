95b380617e7ef7d2cb21d9906ff80d392f7c62d1
hamjiwon <skxo0039@gmail.com>
2026-02-03 22:51:07 +0900

    수박수박수박수박수박수?

---
+++ src/level1/Q12922.java
@@ -0,0 +1,10 @@
+package level1;
+
+public class Q12922 {
+    public String solution(int n) {
+        StringBuilder answer = new StringBuilder();
+
+        if (n % 2 == 0) return answer.append("수박".repeat(n / 2)).toString();
+        else return answer.append("수박".repeat(n / 2) + "수").toString();
+    }
+}
