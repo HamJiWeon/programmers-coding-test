a6c99acf3e6155b8c64fe5c2d62dca7d221cee4f
hamjiwon <skxo0039@gmail.com>
2026-02-04 23:09:58 +0900

    부족한 금액 계산하기

---
+++ src/level1/Q82612.java
@@ -0,0 +1,15 @@
+package level1;
+
+public class Q82612 {
+    public long solution(int price, int money, int count) {
+        long answer = 0;
+
+        for (int i = 1; i <= count; i++) {
+            answer += (long) price * i;
+        }
+
+        if (answer < money) return 0;
+        return answer - money;
+
+    }
+}
