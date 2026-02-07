986fc7e385bead461754f990109f0c6a9ceb684b
hamjiwon <skxo0039@gmail.com>
2026-02-07 18:16:17 +0900

    행렬의 덧셈

---
+++ src/level1/Q12950.java
@@ -0,0 +1,17 @@
+package level1;
+
+public class Q12950 {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+
+        int rows = arr1.length;
+        int cols = arr1[0].length;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                arr1[i][j] += arr2[i][j];
+            }
+        }
+
+        return arr1;
+    }
+}
