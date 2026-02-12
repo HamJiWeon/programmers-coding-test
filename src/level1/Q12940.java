a92b0eb276047ad158ccd3516ec1f820abbf1516
hamjiwon <skxo0039@gmail.com>
2026-02-12 22:02:41 +0900

    최대공약수와 최소공배수

---
+++ src/level1/Q12940.java
@@ -0,0 +1,16 @@
+package level1;
+
+public class Q12940 {
+    public int[] solution(int n, int m) {
+        int[] answer = new int[2];
+
+        for (int i = 1; i <= n && i <= m; i++) {
+            if (n % i == 0 && m % i == 0) {
+                answer[0] = i;
+                answer[1] = n * m / i;
+            }
+        }
+
+        return answer;
+    }
+}
