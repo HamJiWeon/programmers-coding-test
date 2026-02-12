3c7325ea8c8f2b2fa7c4bc8fa1d584b02be2d522
hamjiwon <skxo0039@gmail.com>
2026-02-12 23:21:39 +0900

    크기가 작은 부분 문자열

---
+++ src/level1/Q147355.java
@@ -0,0 +1,16 @@
+package level1;
+
+public class Q147355 {
+    public int solution(String t, String p) {
+        int answer = 0;
+        long number = Long.parseLong(p);
+
+        for (int rt = 0; rt <= t.length() - p.length(); rt++) {
+            int lt = rt + p.length();
+
+            if (Long.parseLong(t.substring(rt, lt)) <= number) answer++;
+        }
+
+        return answer;
+    }
+}
