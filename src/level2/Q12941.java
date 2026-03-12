873b55f967e14f8de97dcc56fbe7824db290f8f8
hamjiwon <skxo0039@gmail.com>
2026-03-12 20:23:46 +0900

    최솟값 만들기

---
+++ src/level2/Q12941.java
@@ -0,0 +1,20 @@
+package level2;
+
+import java.util.*;
+
+public class Q12941 {
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+
+        int len = A.length - 1;
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        for (int i = 0; i <= len; i++) {
+            answer += A[i] * B[len - i];
+        }
+
+        return answer;
+    }
+}
