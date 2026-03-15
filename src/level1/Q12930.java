59047427d4f7162a5686ce658cd6c2b6abc48d56
hamjiwon <skxo0039@gmail.com>
2026-03-15 15:32:23 +0900

    이상한 문자 만들기

---
+++ src/level1/Q12930.java
@@ -0,0 +1,21 @@
+package level1;
+
+public class Q12930 {
+    public String solution(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        int idx = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == ' ') {
+                idx = 0;
+                sb.append(c);
+            } else {
+                sb.append(idx % 2 == 0 ? Character.toUpperCase(c) : Character.toLowerCase(c));
+                idx++;
+            }
+        }
+
+        return sb.toString();
+    }
+}
