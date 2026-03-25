ee75597ed852436def43f58ac26e82652bffe822
hamjiwon <skxo0039@gmail.com>
2026-03-25 15:53:22 +0900

    K번째 수

---
+++ src/level1/Q42748.java
@@ -0,0 +1,33 @@
+package level1;
+
+import java.util.*;
+
+public class Q42748 {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for (int i = 0; i < commands.length; i++) {
+            List<Integer> list = new ArrayList<>();
+            for (int j = commands[i][0] - 1; j < commands[i][1]; j++) {
+                list.add(array[j]);
+            }
+            Collections.sort(list);
+            answer[i] = list.get(commands[i][2] - 1);
+        }
+
+        return answer;
+    }
+
+    // Stream Array.copyOfRange()사용
+    public int[] solutionStream(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for (int i = 0; i < commands.length; i++) {
+            int[] temp = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+            Arrays.sort(temp);
+            answer[i] = temp[commands[i][2] - 1];
+        }
+
+        return answer;
+    }
+}
