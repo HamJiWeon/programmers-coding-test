e7cf6604abfaeb1d05bae06753c20f4e934f0331
hamjiwon <skxo0039@gmail.com>
2026-04-04 17:14:56 +0900

    카드 뭉치

---
+++ src/level1/Q159994.java
@@ -0,0 +1,22 @@
+package level1;
+
+public class Q159994 {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        int idx1 = 0;
+        int idx2 = 0;
+
+        for (String s : goal) {
+            if (idx1 < cards1.length && s.equals(cards1[idx1])) {
+                idx1++;
+            }
+            else if (idx2 < cards2.length && s.equals(cards2[idx2])) {
+                idx2++;
+            }
+            else {
+                return "No";
+            }
+        }
+
+        return "Yes";
+    }
+}
