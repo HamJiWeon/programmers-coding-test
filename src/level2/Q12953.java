92cdb082cafd3ae337a893c0e744c7b8c9af1686
hamjiwon <skxo0039@gmail.com>
2026-04-07 16:07:31 +0900

    N개의 최소공배수

---
+++ src/level2/Q12953.java
@@ -0,0 +1,27 @@
+package level2;
+
+public class Q12953 {
+    public int solution(int[] arr) {
+        int answer = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            answer = LCM(answer, arr[i]);
+        }
+
+        return answer;
+    }
+
+    private int GCD(int a, int b) {
+        while (b != 0) {
+            int r = a % b;
+            a = b;
+            b = r;
+        }
+
+        return a;
+    }
+
+    private int LCM(int a, int b) {
+        return (a * b) / GCD(a, b);
+    }
+}
