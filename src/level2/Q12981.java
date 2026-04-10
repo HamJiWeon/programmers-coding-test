e17704f73f3c911c9418b16f8c5dada8737549bb
hamjiwon <skxo0039@gmail.com>
2026-04-10 15:49:42 +0900

    영어 끝말잇기

---
+++ src/level2/Q12981.java
@@ -0,0 +1,39 @@
+package level2;
+
+import java.util.*;
+
+public class Q12981 {
+    public int[] solution(int n, String[] words) {
+
+        List<String> list = new ArrayList<>();
+
+        list.add(words[0]);
+        for (int i = 1; i < words.length; i++) {
+            if (words[i - 1].charAt(words[i - 1].length() - 1) != words[i].charAt(0)) {
+                return new int[]{(i % n) + 1, (i / n) + 1};
+            } else if (list.contains(words[i])) {
+                return new int[]{(i % n) + 1, (i / n) + 1};
+            }
+
+            list.add(words[i]);
+        }
+
+        return new int[]{0, 0};
+    }
+
+    public int[] solutionSetAndRefactor(int n, String[] words) {
+
+        Set<String> set = new HashSet<>();
+
+        set.add(words[0]);
+        for (int i = 1; i < words.length; i++) {
+            if (words[i - 1].charAt(words[i - 1].length() - 1) != words[i].charAt(0) || set.contains(words[i])) {
+                return new int[]{(i % n) + 1, (i / n) + 1};
+            }
+
+            set.add(words[i]);
+        }
+
+        return new int[]{0, 0};
+    }
+}
